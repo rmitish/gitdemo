dcfd1cf2af73ba50dbb950a23848beb14703b761
Richie Mitish <viorelia.mitish@jetbrains.com>
2022-11-07 21:27:18 +0100

    commit_02
    Signed-off-by: Richie Mitish <viorelia.mitish@jetbrains.com>

--- postgres-sakila-insert-data.sql
+++ postgres-sakila-insert-data.sql
@@ -230,16 +230,14 @@ Values
 ('16','Brunei','2006-02-15 04:44:00.000')
 ;
 Insert into country
+ (country_id,country,last_update)Insert into country
  (country_id,country,last_update)
-Values
-('15','Brazil','2006-02-15 04:44:00.000')
-;
+
 Insert into country
  (country_id,country,last_update)
 Values
 ('173456789','CANADA','2006-02-15 04:44:00.000')
 ;
-Insert into country
  (country_id,country,last_update)
 Values
 ('18','Cambodia','2006-02-15 04:44:00.000')
